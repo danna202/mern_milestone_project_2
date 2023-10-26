2aa43351eea383eebe376a8c77ce4974f08de782
Hannah Mills <millsh56@gmail.com>
2023-10-26 19:23:35 -0400

    setting up dependancies, beggining routes, .env for port

---
+++ mern_milestone_project/src/server.js
@@ -0,0 +1,32 @@
+// DEPENDENCIES 
+const express = require('express')
+const app = express()
+const methodOverride = require('method-override')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.port
+console.log(PORT)
+
+// MIDDLEWARE
+// app.set('views', __dirname + '/views') 
+// app.set('view engine', 'jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+// app.use(express.urlencoded({extended: true}))
+// app.use(methodOverride('_method'))
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('TEST SETUP FBI WANTED API APP')
+})
+
+// 404 Page remember! no routes under this
+app.get('*', (req, res) => {
+  res.render('404')
+})
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+})
