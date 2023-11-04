ad7d4563a464981241f8d15d985aa53bcfbb3a60
Hannah Mills <millsh56@gmail.com>
2023-11-04 18:22:29 -0400

    building out sign in form

--- mern_milestone_project/src/components/SignIn.js
+++ mern_milestone_project/src/components/SignIn.js
@@ -5,3 +5,32 @@ function SignIn() {
   const [password, setPassword] = useState('');
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  return (
+    <div className="sign-in">
+      <h2>Sign In</h2>
+        <form>
+          <div>
+            <label>Username:</label>
+            <input
+              type="username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="button" onClick={handleSignIn}>
+            Sign In
+          </button>
+        </form>
+    </div>
+  );
+}
+
+export default SignIn;
