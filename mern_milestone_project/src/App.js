31c371a71790b0c7365d7d73594d2a810b04e131
Hannah Mills <millsh56@gmail.com>
2023-11-04 17:17:40 -0400

    refactoring to utilize react router

--- mern_milestone_project/src/App.js
+++ mern_milestone_project/src/App.js
@@ -1,20 +1,23 @@
-import { useState, useEffect } from 'react';
-import fetch from 'node-fetch';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import WantedCard from './components/WantedCard';
-import SearchBar from './components/SearchBar';
+// import { useState, useEffect } from 'react';
+// import fetch from 'node-fetch';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+// import WantedCard from './components/WantedCard';
+// import SearchBar from './components/SearchBar';
 import Gallery from './components/Gallery';
 
-function App () {
 
-return(
-  <div className='App'>
-    <Gallery/>
-  </div>
-);
+function App() {
+  return (
+    <BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/profile" component={UserProfile} />
+        <Route path="/gallery" component={Gallery} />
+      </Routes>
+    </Router>
+    </BrowserRouter>
+  );
 }
-
-
 
 export default App;
 
---
+++ mern_milestone_project/src/components/UserInfo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function ProfileInfo({ user }) {
+  return (
+    <div className="profile">
+      <h2>User Profile Information</h2>
+      <div>
+        <p>Name:</p> {user.name}
+      </div>
+      <div>
+        <p>Email:</p> {user.email}
+      </div>
+      <div>
+        <p>Username:</p> {user.username}
+      </div>
+      <div>
+        <p>Location:</p> {user.location}
+      </div>
+    
+    </div>
+  );
+}
+
+export default ProfileInfo;
